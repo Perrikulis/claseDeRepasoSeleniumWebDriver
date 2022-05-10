8e7986e2fcad9ea37eafd8bacbef781cb42922c1
Jennifer Ramirez <jxr20920@dexcom.com>
2022-05-09 19:02:10 -0500

    Merging merge changes from PRs

--- src/main/java/Pages/LandingPage.java
+++ src/main/java/Pages/LandingPage.java
@@ -19,4 +19,22 @@ public class LandingPage extends BasePage {
         List<WebElement> menuOptions = webDriver.findElements(By.cssSelector(menuOptionsCSSLocator));
         return menuOptions;
     }
+
+    private final String userNameTextBox = ".col-md-9.col-sm-12 > #userName";
+    private final String userMailTextBox = ".col-md-9.col-sm-12 > #userEmail";
+    private final String currentAddressTextBox = ".col-md-9.col-sm-12 > #currentAddress";
+    private final String permanentAddressTextBox = ".col-md-9.col-sm-12 > #permanentAddress";
+    private final String button = ".text-right.col-md-2.col-sm-12 > button";
+
+    public WebElement getUserNameTextBox(){
+        return webDriver.findElement(By.cssSelector(userNameTextBox));
+    }
+
+    public WebElement getUserMailTextBox(){ return webDriver.findElement(By.cssSelector(userMailTextBox));  }
+
+    public WebElement getcurrentAddressTextBox(){ return webDriver.findElement(By.cssSelector(currentAddressTextBox));}
+
+    public WebElement getpermanentAddressTextBox(){ return webDriver.findElement(By.cssSelector(permanentAddressTextBox));}
+
+    public WebElement getButton (){ return webDriver.findElement(By.cssSelector(button));}
 }
--- src/main/java/Steps/LandingsSteps.java
+++ src/main/java/Steps/LandingsSteps.java
@@ -2,6 +2,8 @@ package Steps;
 
 import org.openqa.selenium.WebDriver;
 import Pages.LandingPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class LandingsSteps extends BaseSteps{
     LandingPage landingPage = new LandingPage(webDriver);
@@ -9,6 +11,33 @@ public class LandingsSteps extends BaseSteps{
     public LandingsSteps(WebDriver webDriver) {
         super(webDriver);
     }
+
+    public boolean userNameTextBoxIsDisplayed(){
+        return landingPage.getUserNameTextBox().isDisplayed();
+
+    }
+
+    public boolean userMailTextBoxIsDisplayed(){
+        return landingPage.getUserMailTextBox().isDisplayed();
+
+    }
+
+    public boolean currentAddressTextBoxIsDisplayed(){
+        return landingPage.getcurrentAddressTextBox().isDisplayed();
+
+    }
+
+
+    public boolean permanentAddressTextBoxisDisplayed(){
+        return landingPage.getpermanentAddressTextBox().isDisplayed();
+
+    }
+
+    public boolean buttonIsDisplayed(){
+        return landingPage.getButton().isDisplayed();
+
+    }
+
     //Dar click en la opcion Elements
     public void clickOption (int option){
         landingPage.getMenuOptions().get(option).click();

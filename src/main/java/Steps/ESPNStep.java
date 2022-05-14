f3d54eee0642e29f6060d9a1f8eb090dfe7d8691
Jennifer Ramirez <jxr20920@dexcom.com>
2022-05-14 11:58:39 -0500

    Reporter with line break and merge fixes in ESPNTests and Steps
    Reporter with line break and merge fixes

--- src/main/java/Steps/ESPNStep.java
+++ src/main/java/Steps/ESPNStep.java
@@ -19,34 +19,41 @@ public class ESPNStep extends BaseSteps{
     }
 
     public String getMexicoConcacafElements(){
-        Reporter.log("Obteniendo valores de CONCACAF");
         WebElement paisesElement = page.MexicoConcacaftitle();
+        Reporter.log("Step: Obteniendo valores de " + paisesElement.getText() + "<br>");
         return paisesElement.getText();
     }
 
     public String getEuropeTitle(){
         WebElement paisesElement = page.europaTitle();
+        Reporter.log("Step: Obteniendo valores de " + paisesElement.getText() + "<br>");
         return paisesElement.getText();
     }
     public String getInternationalTitle(){
         WebElement paisesElement = page.internationalTitle();
+        Reporter.log("Step: Obteniendo valores de " + paisesElement.getText() + "<br>");
         return paisesElement.getText();
     }
     public String getSoutAmericaTitle(){
         WebElement paisesElement = page.soutAmericaTitle();
+        Reporter.log("Step: Obteniendo valores de " + paisesElement.getText() + "<br>");
         return paisesElement.getText();
     }
 
     public List<String> getElements(){
+        Reporter.log("Step: Obteniendo lista de elementos de Mexico CONCACAF<br>");
         return page.MexicoConcacafElements().stream().map(x -> x.getText()).collect(Collectors.toList());
     }
     public List<String> getElementsInternational(){
+        Reporter.log("Step: Obteniendo lista de elementos de International<br>");
         return page.internationalElements().stream().map(x -> x.getText()).collect(Collectors.toList());
     }
     public List<String> getElementsEurope(){
+        Reporter.log("Step: Obteniendo lista de elementos de europe<br>");
         return page.europeElements().stream().map(x -> x.getText()).collect(Collectors.toList());
     }
     public List<String> getElementsSouthAmerica(){
+        Reporter.log("Step: Obteniendo lista de elementos de South America<br>");
         return page.southAmericaElements().stream().map(x -> x.getText()).collect(Collectors.toList());
     }
 }
--- src/test/java/ESPNTest.java
+++ src/test/java/ESPNTest.java
@@ -7,66 +7,49 @@ public class ESPNTest extends BaseTest{
     ESPNStep ESPNStep = new ESPNStep(this.webDriver);
 
     @Test(description = "Testing teams in each tournaments in espn.com.mx", groups = {"ESPNTests", "SmokeTest"})
-
     public void testFutbolCategory() {
+        Reporter.log("Opening: https://www.espn.com.mx/futbol/torneos <br>");
         webDriver.get("https://www.espn.com.mx/futbol/torneos");
-        Reporter.log(ESPNStep.getMexicoConcacafElements());
+        Reporter.log("Category: " + ESPNStep.getMexicoConcacafElements() + "<br>");
         System.out.println(ESPNStep.getMexicoConcacafElements());
-        Reporter.log(ESPNStep.getElements().size() + " Elementos");
+        Reporter.log("Numero de elementos en esta lista: " + ESPNStep.getElements().size() + "<br>");
         System.out.println(ESPNStep.getElements().size() + " Elementos");
         for (String team:ESPNStep.getElements()){
-            Reporter.log(team);
+            Reporter.log(team + "<br>");
         }
         System.out.println(ESPNStep.getElements());
-        Reporter.log(" =============    =============");
+        Reporter.log(" =============    =============<br>");
         System.out.println(" =============    =============");
-        for (String team:ESPNStep.getElements()
-             ) {
-            Reporter.log(team);
-        }
-        Reporter.log("=============    =============");
-        System.out.println("=============    =============");
-        Reporter.log(ESPNStep.getEuropeTitle());
+
+        Reporter.log("Categoria: " + ESPNStep.getEuropeTitle() + "<br>");
         System.out.println(ESPNStep.getEuropeTitle());
-        Reporter.log(ESPNStep.getElementsEurope().size() + " Elementos");
+        Reporter.log("Numero de elementos en esta lista: " + ESPNStep.getElementsEurope().size() + "<br>");
         System.out.println(ESPNStep.getElementsEurope().size() + " Elementos");
         for (String team:ESPNStep.getElementsEurope()){
-            Reporter.log(team);
+            Reporter.log(team + "<br>");
         }
         System.out.println(ESPNStep.getElementsEurope());
-        Reporter.log(" =============    =============");
-        System.out.println(" =============    =============");
-        for (String team: ESPNStep.getElementsEurope()
-             ) {
-            Reporter.log(team);
-        }
-        System.out.println(ESPNStep.getElementsEurope());
-        Reporter.log("=============    =============");
+        Reporter.log("=============    =============<br>");
         System.out.println("=============    =============");
-        Reporter.log(ESPNStep.getInternationalTitle());
+
+        Reporter.log("Categoria: " + ESPNStep.getInternationalTitle() + "<br>");
         System.out.println(ESPNStep.getInternationalTitle());
-        Reporter.log(ESPNStep.getElementsInternational().size() + " Elementos");
+        Reporter.log("Numero de elementos de esta categoria: " + ESPNStep.getElementsInternational().size() + "<br>");
         System.out.println(ESPNStep.getElementsInternational().size() + " Elementos");
         for (String team:ESPNStep.getElementsInternational()){
-            Reporter.log(team);
+            Reporter.log(team + "<br>");
         }
         System.out.println(ESPNStep.getElementsInternational());
-        Reporter.log(" =============    =============");
+        Reporter.log(" =============    =============<br>");
         System.out.println(" =============    =============");
-        for (String team : ESPNStep.getElementsInternational()
-             ) {
-            Reporter.log(team);
-        }
-        System.out.println(ESPNStep.getElementsInternational());
-        Reporter.log("=============    =============");
-        System.out.println("=============    =============");
-        Reporter.log(ESPNStep.getSoutAmericaTitle());
+
+        Reporter.log("Categoria: " + ESPNStep.getSoutAmericaTitle() + "<br>");
         System.out.println(ESPNStep.getSoutAmericaTitle());
-        Reporter.log(ESPNStep.getElementsSouthAmerica().size() + " Elementos");
+        Reporter.log("Numero de elementos en categoria: " + ESPNStep.getElementsSouthAmerica().size() + "<br>");
         System.out.println(ESPNStep.getElementsSouthAmerica().size() + " Elementos");
         for (String team : ESPNStep.getElementsSouthAmerica()
              ) {
-            Reporter.log(team);
+            Reporter.log(team + "<br>");
         }
         System.out.println(ESPNStep.getElementsSouthAmerica());
     }
